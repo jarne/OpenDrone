85d2d7488395cc5094d34980449e64a9c0dc957e
Jarne <jarne@users.noreply.github.com>
2019-04-20 12:20:45 +0200

    Initial commit

---
+++ OpenDrone.ino
@@ -0,0 +1,33 @@
+/**
+ * OpenDrone
+ * 
+ * App-basierte Drohnen-Steuerung mit dem NodeMCU
+ * 
+ * Schul-Projekt 2019
+ * 
+ * Programm v0.0.1
+ * 
+ * (C) 2019 Jarne Jost
+ */
+
+/* Bibliotheken */
+
+
+
+/* gesetzte Einstellungen */
+
+
+
+/* globale Variablen */
+
+
+
+/* Hauptfunktionen */
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    Serial.println("Hallo, das ist ein Test!");
+}
