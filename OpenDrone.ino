3c4fa917a0efdb4fb27b70ca1d3e5b5fe94901b3
Jarne <jarne@users.noreply.github.com>
2019-05-13 15:05:12 +0200

    Got gyro sensor basically working

--- OpenDrone.ino
+++ OpenDrone.ino
@@ -12,22 +12,48 @@
 
 /* Bibliotheken */
 
+#include <Arduino.h>
 
+#include <I2Cdev.h>
+#include <MPU6050.h>
 
 /* gesetzte Einstellungen */
 
-
+#define SCL_PIN 14
+#define SDA_PIN 12
 
 /* globale Variablen */
+
+MPU6050 mpu6050;
+
+int16_t gx, gy, gz;
+
+/* Kommunikation mit Gyro-Sensor */
 
 
 
 /* Hauptfunktionen */
 
 void setup() {
+    Wire.begin(SDA_PIN, SCL_PIN);
+
+    mpu6050.initialize();
+
     Serial.begin(9600);
+
+    Serial.println(mpu6050.testConnection() ? "Verbindung zum MPU OK!" : "Verbindung zum MPU FEHLGESCHLAGEN!");
 }
 
 void loop() {
-    Serial.println("Hallo, das ist ein Test!");
+    //mpu6050.getRotation(&gx, &gy, &gz);
+    mpu6050.getAcceleration(&gx, &gy, &gz); // TODO: Rotation und Acceleration wurden in der Library vertauscht !!!
+
+    Serial.print(gx);
+    Serial.print(" | ");
+    Serial.print(gy);
+    Serial.print(" | ");
+    Serial.print(gz);
+    Serial.println();
+
+    delay(1000);
 }
